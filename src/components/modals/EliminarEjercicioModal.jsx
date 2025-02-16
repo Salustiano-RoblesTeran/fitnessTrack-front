30fb2a432e22892d772a6c4ae3e74ddac3fa5a8d
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2025-02-16 18:50:41 -0300

    modifique una ruta que me habia quedado de la rama dev (local)

--- src/components/modals/EliminarEjercicioModal.jsx
+++ src/components/modals/EliminarEjercicioModal.jsx
@@ -19,7 +19,7 @@ const EliminarEjercicioModal = ({ show, handleClose, ejercicioId }) => {
         setLoading(true);
 
         try {
-            const response = await fetch(`http://localhost:3000/api/ejercicio/${ejercicioId}/eliminar`, {
+            const response = await fetch(`https://fitnesstrack-back.onrender.com/api/ejercicio/${ejercicioId}/eliminar`, {
                 method: 'post',
                 headers: {
                     'x-token': token,
